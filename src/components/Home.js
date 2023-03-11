c7789e5cec1e7b7be087afb663dd5f784e2028c0
CodeMorecamp <71688722+CodeMorecamp@users.noreply.github.com>
2023-03-11 23:11:36 +0100

    Update to Replies component and logic, Replies, added Like and Comment components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import Nav from './Nav'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
     const [thread, setThread]= useState("")
-
+    const [threadList, setThreadList] = useState([]);
     const navigate = useNavigate();
 
     //👇🏻 The useEffect Hook
@@ -12,13 +13,38 @@ const Home = () => {
             if (!localStorage.getItem("_id")) {
                 navigate("/");
             } else {
-                console.log("Authenticated");
+                fetch("http://localhost:4000/api/all/threads")
+                .then((res) => res.json())
+                .then((data) => setThreadList(data.threads))
+                .catch((err) => console.error(err));
             }
         };
         checkUser();
     }, [navigate]);
+
+    const createThread = () => {
+        fetch("http://localhost:4000/api/create/thread", {
+            method: "POST",
+            body: JSON.stringify({
+                thread,
+                userId: localStorage.getItem("_id"),
+            }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                alert(data.message);
+                setThreadList(data.threads)
+            })
+            .catch((err) => console.error(err));
+    };
+
     const handleSubmit = (e) => {
         e.preventDefault()
+        //calls the function
+        createThread();
         console.log([thread])
         setThread("")
     }
@@ -40,6 +66,21 @@ const Home = () => {
          </div>
         <button className='homeBtn'>Create Thread</button>
        </form>
+       <div className='thread__container'>
+                {threadList.map((thread) => (
+                    <div className='thread__item' key={thread.id}>
+                        <p>{thread.title}</p>
+                        <div className='react__container'>
+                            <Likes numberOfLikes={thread.likes.length} threadId={thread.id} />
+                            <Comments
+                                numberOfComments={thread.replies.length}
+                                threadId={thread.id}
+                                title={thread.title}
+                            />
+                        </div>
+                    </div>
+                ))}
+            </div>
     </main>     
     </>
   )
